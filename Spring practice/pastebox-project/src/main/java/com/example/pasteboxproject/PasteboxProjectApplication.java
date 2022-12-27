c84e98d8c13227b556c307502d5d898e48eb14f1
welderdefender <welderdefender@yandex.ru>
2022-12-27 12:51:14 +0300

    init

---
+++ Spring practice/pastebox-project/src/main/java/com/example/pasteboxproject/PasteboxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasteboxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteboxProjectApplication.class, args);
+	}
+
+}
---
+++ Spring practice/pastebox-project/src/test/java/com/example/pasteboxproject/PasteboxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pasteboxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
